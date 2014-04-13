b7e2725eae33e2d9bd85050a2748274501387bdb
Dean Netherton <antispam@netherton.org>
2014-04-13 11:39:38 +1000

    Added license

--- Sample/Properties/AssemblyInfo.cs
+++ Sample/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using System.Reflection;
+﻿// This file is part of iRacingSDK.
+//
+// Copyright 2014 Dean Netherton
+// https://github.com/vipoo/iRacingSDK.Net
+//
+// iRacingSDK is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// iRacingSDK is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with iRacingSDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
