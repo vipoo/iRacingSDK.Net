dabfba7017d20b1c5c3750574be67df5caca386a
Dean Netherton <antispam@netherton.org>
2014-04-13 11:10:15 +1000

    Added a very simple sample on using the sdk - more to come

---
+++ Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using iRacingSDK;
+
+namespace SpikeIRSDK
+{
+	class MainClass
+	{
+		public unsafe static void Main(string[] args)
+		{
+			var iRacing = new Feed();
+			if(!iRacing.Connect())
+				throw new Exception("Unable to connect to iRacing server");
+
+			iRacing.OnSessionInfo( s => Console.WriteLine("New session data "));
+			var sessionInfo = iRacing.SessionInfo;
+
+			Console.WriteLine(sessionInfo);
+
+			foreach(var data in iRacing.DataFeed)
+			{
+				Console.WriteLine("Tick, Session Time: " + data["TickCount"] + ", " + data["SessionTime"]);
+			}
+		}
+	}
+}
