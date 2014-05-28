10d496e8a93f175ba30dab882a2277edd4b29acd
Dean Netherton <antispam@netherton.org>
2014-05-28 18:31:14 +1000

    Added some guards to DataSample properties - Telemetry and SessionData.  Will raise exception if attempt to read these before connection to iracing

--- DataFeed/DataSample.cs
+++ DataFeed/DataSample.cs
@@ -24,12 +24,49 @@ namespace iRacingSDK
 	[Serializable]
     public class DataSample
     {
+        private Telemetry telemetry;
+        private SessionData sessionData;
+
         public static readonly DataSample YetToConnected = new DataSample { IsConnected = false };
         public DataSample LastSample { get; set; }
 
         public bool IsConnected { get; internal set; }
-        public SessionData SessionData { get; set; }
-        public Telemetry Telemetry { get; set; }
+        public SessionData SessionData
+        {
+            get
+            {
+                if (!IsConnected)
+                    throw new Exception("Attempt to read session data before connection to iRacing");
+
+                if (sessionData == null)
+                    throw new Exception("SessionData is null");
+
+                return sessionData;
+            }
+            set 
+            {
+                sessionData = value;
+            }
+        }
+
+        public Telemetry Telemetry
+        {
+            get
+            {
+                if (!IsConnected)
+                    throw new Exception("Attempt to read telemetry data before connection to iRacing");
+
+                if (telemetry == null)
+                    throw new Exception("Telemetry is null");
+
+                return telemetry;
+            }
+            set
+            {
+                telemetry = value;
+            }
+        }
+        
         public Dictionary<string, string> TelemetryDescription { get; internal set; }
     }
 }
