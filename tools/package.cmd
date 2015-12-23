8c762e95203225243fd587e7375d93a07a5b02de
Dean Netherton <dean.netherton@gmail.com>
2015-12-23 21:00:57 +1100

    TargetFramework: targeting frmwrk 4.0, client

--- tools/package.cmd
+++ tools/package.cmd
@@ -4,3 +4,4 @@ SET ProjectDir=%~dp0..\
 msbuild %ProjectDir%iRacingSDK.csproj -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal /p:Configuration=Release
 
 %ProjectDir%tools\nuget.exe pack %ProjectDir%iRacingSDK.nuspec -Prop Configuration=release -Verbosity detail -Version %APPVEYOR_BUILD_VERSION%
+
